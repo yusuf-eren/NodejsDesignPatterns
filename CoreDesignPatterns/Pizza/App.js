c0c11150edfc515a8febab998a68245b2c2fe2ff
Yusuf Eren <erenyusuf170@gmail.com>
2022-08-30 21:31:43 +0300

    x

--- CoreDesignPatterns/Pizza/App.js
+++ CoreDesignPatterns/Pizza/App.js
@@ -1,9 +1,9 @@
-const Pizza = require('./Pizza');
+const Pizza = require("./Pizza");
 const pepperoni = new Pizza();
 
-pepperoni.setBase('Whole Wheat');
-pepperoni.setSauce('Tomato Basil');
-pepperoni.setCheese('Smoked Mozzarella');
-pepperoni.setToppings(['Pepperoni', 'Basil']);
+pepperoni.setBase("Whole Wheat");
+pepperoni.setSauce("Tomato Basil");
+pepperoni.setCheese("Smoked Mozzarella");
+pepperoni.setToppings(["Pepperoni", "Basil"]);
 
 pepperoni.bake();
--- CoreDesignPatterns/Pizza/Pizza.js
+++ CoreDesignPatterns/Pizza/Pizza.js
@@ -1,9 +1,9 @@
 function Pizza() {
-  this.pizza = {}
-  this.setBase = base => this.pizza.base = base;
-  this.setSauce = sauce => this.pizza.sauce = sauce;
-  this.setCheese = cheese => this.pizza.cheese = cheese;
-  this.setToppings = toppings => this.pizza.toppings = toppings;
+  this.pizza = {};
+  this.setBase = (base) => (this.pizza.base = base);
+  this.setSauce = (sauce) => (this.pizza.sauce = sauce);
+  this.setCheese = (cheese) => (this.pizza.cheese = cheese);
+  this.setToppings = (toppings) => (this.pizza.toppings = toppings);
   this.bake = () => console.log(this.pizza);
 }
 
