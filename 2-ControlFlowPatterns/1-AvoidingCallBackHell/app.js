b91cad3c4144850563762aa4f695aeddda6c26a3
Yusuf Eren <erenyusuf170@gmail.com>
2022-08-30 23:54:30 +0300

    prettier

--- 2-ControlFlowPatterns/1-AvoidingCallBackHell/app.js
+++ 2-ControlFlowPatterns/1-AvoidingCallBackHell/app.js
@@ -1,50 +1,41 @@
-const {
-  readdir,
-  readFile,
-  writeFile
-} = require("fs");
-const {
-  join
-} = require("path");
-const {
-  promisify
-} = require("util");
+const { readdir, readFile, writeFile } = require("fs");
+const { join } = require("path");
+const { promisify } = require("util");
 const inbox = join(__dirname, "inbox");
 const outbox = join(__dirname, "outbox");
 
-const reverseText = str =>
-  Promise.resolve(str
-    .split("")
-    .reverse()
-    .join(""));
+const reverseText = (str) => Promise.resolve(str.split("").reverse().join(""));
 
 const readdirAsync = promisify(readdir); // Promisified version of readdir
 const readFileAsync = promisify(readFile);
 const writeFileAsync = promisify(writeFile);
 
 // Read and reverse contents of text files in a directory
-async function readReverseWrite(file) {
+async function* getFilesIterator() {
   try {
-    const read = await readFileAsync(join(inbox, file), "utf8");
-    const reverse = await reverseText(read);
-    await writeFileAsync(join(outbox, file), reverse);
+    const readDirectory = await readdirAsync(inbox);
+    for (const file of readDirectory) {
+      yield file;
+    }
   } catch (error) {
     throw Error(error);
-  } finally {
-    console.log(`${file} has been processed!`);
   }
 }
 
-async function processFiles() {
+async function process() {
   try {
-    const getAllFiles = await readdirAsync(inbox);
-    const processAllFiles = getAllFiles.map(file => readReverseWrite(file));
-    await Promise.all(processAllFiles);
+    const allFiles = await getFilesIterator(); // AsyncGenerator Object
+    for await (const file of allFiles) {
+      const read = await readFileAsync(join(inbox, file), "utf8");
+      const reverse = await reverseText(read);
+      await writeFileAsync(join(outbox, file), reverse);
+      console.log(`${file} has been processed!`);
+    }
   } catch (error) {
-    console.log(error); // Handle errors
+    console.log(error);
   } finally {
     console.log("*** All files processed ***");
   }
 }
 
-processFiles();
+process();
