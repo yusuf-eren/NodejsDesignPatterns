50112586721b5e93e624fd6475ca48a127fdac39
Yusuf Eren <erenyusuf170@gmail.com>
2022-08-30 23:53:41 +0300

    Async-Await

--- 2-ControlFlowPatterns/1-AvoidingCallBackHell/app.js
+++ 2-ControlFlowPatterns/1-AvoidingCallBackHell/app.js
@@ -1,22 +1,50 @@
-const { readdir, readFile, writeFile } = require("fs");
-const { join } = require("path");
+const {
+  readdir,
+  readFile,
+  writeFile
+} = require("fs");
+const {
+  join
+} = require("path");
+const {
+  promisify
+} = require("util");
 const inbox = join(__dirname, "inbox");
 const outbox = join(__dirname, "outbox");
 
-const reverseText = (str) => str.split("").reverse().join("");
+const reverseText = str =>
+  Promise.resolve(str
+    .split("")
+    .reverse()
+    .join(""));
+
+const readdirAsync = promisify(readdir); // Promisified version of readdir
+const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
 
 // Read and reverse contents of text files in a directory
-readdir(inbox, (error, files) => {
-  if (error) return console.log("Error: Folder inaccessible");
-  files.forEach((file) => {
-    readFile(join(inbox, file), "utf8", (error, data) => {
-      if (error) return console.log("Error: File error");
-      else {
-        writeFile(join(outbox, file), reverseText(data), (error) => {
-          if (error) return console.log("Error: File could not be saved!");
-          console.log(`${file} was successfully saved in the outbox!`);
-        });
-      }
-    });
-  });
-});
+async function readReverseWrite(file) {
+  try {
+    const read = await readFileAsync(join(inbox, file), "utf8");
+    const reverse = await reverseText(read);
+    await writeFileAsync(join(outbox, file), reverse);
+  } catch (error) {
+    throw Error(error);
+  } finally {
+    console.log(`${file} has been processed!`);
+  }
+}
+
+async function processFiles() {
+  try {
+    const getAllFiles = await readdirAsync(inbox);
+    const processAllFiles = getAllFiles.map(file => readReverseWrite(file));
+    await Promise.all(processAllFiles);
+  } catch (error) {
+    console.log(error); // Handle errors
+  } finally {
+    console.log("*** All files processed ***");
+  }
+}
+
+processFiles();
