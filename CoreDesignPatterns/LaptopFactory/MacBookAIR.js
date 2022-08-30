b7e7e7b7312696053958d341d3d869003822c385
Yusuf Eren <erenyusuf170@gmail.com>
2022-08-30 21:05:49 +0300

    x

--- CoreDesignPatterns/LaptopFactory/MacBookAIR.js
+++ CoreDesignPatterns/LaptopFactory/MacBookAIR.js
@@ -2,7 +2,7 @@ const Macbook = require("./Laptop");
 
 class MacBookAIR {
   constructor(serialNum) {
-    return new Macbook(serialNum, "MacBook AIR", "M2", "8 GB", 7);
+    return new Macbook(serialNum, "MacBook AIR", "M2", "8 GB", 13.3);
   }
 }
 
